9e01a5b15ab89383c5d9ef0be1a7826630503c32
Frank Pagan <frank@cocreate.app>
2022-12-30 22:16:29 +0000

    fix: update config sources to use template  braces with entry on src

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -6,13 +6,12 @@ module.exports = {
     },
     "sources": [
         {
-            "entry": "./docs/index.html",
             "collection": "files",
             "document": {
                 "_id": "637c9bef5b8e62354f58ec96",
                 "name": "index.html",
                 "path": "/docs/element-config/index.html",
-                "src": "{{source}}",
+                "src": "{{./docs/index.html}}",
                 "domains": [
                     "*",
                     "general.cocreate.app"
